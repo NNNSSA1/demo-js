806595e9cecf3e16603d710f2f5e2d08ef051e45
wade <352912788@qq.com>
2018-05-11 11:41:09 +0800

    1

--- 无缝轮播/main.js
+++ 无缝轮播/main.js
@@ -1,24 +1,23 @@
 $(document).ready(function () {
-    let n 
+    let n
     let size
-    Init.call()
+    Init()
     setInterval(() => {
-        MakeLeave($(`.window>div:nth-child(${x(n)})`))                    //`插指法。。。` 
-        .one('transitionend', function (e) { MakeStay($(e.currentTarget)) })    
-        MakeCurrent($(`.window>div:nth-child(${x(n+1)})`))
+        MakeLeave(getChild(n)).one('transitionend', function (e) { MakeStay($(e.currentTarget)) })  //`插指法。。。` 
+        MakeCurrent(getChild(n + 1))
         n += 1
     }, 2000)
 
 
 
-    
+
     //工具函数
     function Init() {
         n = 1
         size = $('.window>div').length
         $(`.window>div:nth-child(${n})`).addClass('current').siblings().addClass('stay')
     }
-    function x(n){
+    function x(n) {
         if (n > size) {
             n = n % size
             if (n === 0) {
@@ -27,17 +26,20 @@ $(document).ready(function () {
         }
         return n
     }
-    function MakeCurrent($node){                                            //状态机
+    function MakeCurrent($node) {                                            //状态机
         $node.removeClass('stay leave').addClass('current')
-        return  $node
+        return $node
     }
-    function MakeLeave($node){
+    function MakeLeave($node) {
         $node.removeClass('current stay').addClass('leave')
-        return  $node
+        return $node
     }
-    function MakeStay($node){
-        $node.removeClass('leave').addClass('stay') 
-        return  $node
+    function MakeStay($node) {
+        $node.removeClass('leave').addClass('stay')
+        return $node
+    }
+    function getChild(n) {
+        return $(`.window>div:nth-child(${x(n)})`)
     }
 })
 
